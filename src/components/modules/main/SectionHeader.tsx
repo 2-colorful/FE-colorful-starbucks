596403394f32de9c5fb4caab5b7a04f0abf68aac
HoChoRoo <rttkhc0304@gmail.com>
2025-04-22 14:42:20 +0900

    🐛 fix: 메인 페이지 이벤트 부분 API 연동 및 컴포넌트화

--- src/components/modules/main/SectionHeader.tsx
+++ src/components/modules/main/SectionHeader.tsx
@@ -5,11 +5,14 @@ type SectionHeaderProps = {
   eventUuid: string;
 };
 
-export default function SectionHeader({ title }: SectionHeaderProps) {
+export default function SectionHeader({
+  title,
+  eventUuid,
+}: SectionHeaderProps) {
   return (
     <div className='flex items-center justify-between mb-[30px]'>
       <h2 className='text-title2'>{title}</h2>
-      <MoreButton href='' title='더보기' />
+      <MoreButton href={`/events?eventId=${eventUuid}`} title='더보기' />
     </div>
   );
 }
